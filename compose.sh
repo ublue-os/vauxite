5d6f2bc98af834b66207ec2a99af6dd00d24883f
Jeffrey Serio <23226432+hyperreal64@users.noreply.github.com>
2022-10-24 20:47:39 -0500

    Initial commit of new Vauxite

---
+++ compose.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -eu
+CACHE=/var/cache/ostree
+REPO=/var/tmp/repo
+
+mkdir -p $CACHE
+
+if [ ! -d $REPO/objects ]; then
+    ostree --repo=$REPO init --mode=archive-z2
+fi
+
+rpm-ostree compose tree --unified-core --cachedir=$CACHE --repo=$REPO vauxite-desktop.yaml
