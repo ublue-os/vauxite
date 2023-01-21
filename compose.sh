fce5745f9cf488ef48aa856c483b661ce6d93a72
Jeffrey Serio <23226432+hyperreal64@users.noreply.github.com>
2023-01-21 12:46:28 -0600

    Remove other DE treefiles; update compose.sh to build container image

--- compose.sh
+++ compose.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env sh
 
 set -eu
-CACHE=/var/cache/ostree
-REPO=/var/tmp/repo
 
-mkdir -p $CACHE
-
-if [ ! -d $REPO/objects ]; then
-    ostree --repo=$REPO init --mode=archive-z2
-fi
-
-rpm-ostree compose tree --unified-core --cachedir=$CACHE --repo=$REPO fedora-vauxite.yaml
+rpm-ostree compose image --initialize --format=registry fedora-vauxite.yaml ghcr.io/ublue-os/vauxite:latest
